cc81de05fc326558fe539b9195a986885684b045
Karina Gazca <karinagazca.kg@gmail.com>
2025-09-08 11:36:31 -0600

    Neural network (autoencoder)

---
+++ autoencoder/nn.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from keras.models import Model
+from tensorflow.keras.models import load_model
+
+
+class Autoencoder:
+    def __init__(self, input_neurons, input_size, embedding_size=200,
+                 optimizer='adam', metrics=['CosineSimilarity'], loss=tf.keras.losses.mean_squared_logarithmic_error):
+        #hyperparameters
+        self.optimizer = optimizer
+        self.metrics = metrics
+        self.loss = loss
+        #create the nn
+        self.autoencoder = tf.keras.models.Sequential()
+        self.autoencoder.add(tf.keras.layers.Dense(input_neurons, input_shape=(input_size,)))
+        self.autoencoder.add(tf.keras.layers.Dropout(0.3))
+        #encoder
+        encoder = tf.keras.layers.Dense(embedding_size, activation=tf.nn.sigmoid)
+        self.autoencoder.add(encoder)
+        #decoder
+        decoder = tf.keras.layers.Dense(input_size, activation=tf.nn.sigmoid)
+        self.autoencoder.add(decoder)
+        #save the layers indexes to encode and decode later
+        self.index_last_encoder_layer = self.autoencoder.layers.index(encoder)
+        self.index_decoder_layer = self.autoencoder.layers.index(decoder)
+        self.autoencoder.summary()
+
+    def fit(self, X_train, y_train, X_val, y_val, epochs=300, batch_size=256, shuffle=True):
+        self.autoencoder.compile(optimizer=self.optimizer, metrics=self.metrics,loss=self.loss)
+        # Train the nn
+        history = self.autoencoder.fit(X_train, y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        shuffle=shuffle,
+                        validation_data=(X_val, y_val))
+        return history
+    
+    def save(self, name='model.h5'):
+        self.autoencoder.save(name)
+
+    def load_model(self, name='model.h5'):
+        self.autoencoder = load_model(name)
+        return self.autoencoder
+   
+    def predict(self, X):
+        y_pred = self.autoencoder.predict(X)
+        return y_pred
+
+    def encode(self):
+        self.encoder = Model(inputs=self.autoencoder.input, outputs=self.autoencoder.get_layer(index=self.index_last_encoder_layer).output)
+        return self.encoder
+
+    def decode(self):
+        self.decoder = Model(inputs=self.autoencoder.get_layer(index=self.index_last_encoder_layer).output, outputs=self.autoencoder.get_layer(index=self.index_decoder_layer).output)
+        return self.decoder
